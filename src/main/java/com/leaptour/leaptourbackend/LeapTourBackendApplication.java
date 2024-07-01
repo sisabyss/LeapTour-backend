259154c5a26d2575ab33236f2e2aac0287e3efde
sisabyss <xiwei_tao@outlook.com>
2024-07-01 20:00:04 +0800

    init commmit

---
+++ src/main/java/com/leaptour/leaptourbackend/LeapTourBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leaptour.leaptourbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapTourBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapTourBackendApplication.class, args);
+	}
+
+}
